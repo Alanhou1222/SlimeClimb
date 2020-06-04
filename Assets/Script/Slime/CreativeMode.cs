482c8d095dd004903e3742ccbf660c16b3e85aa8
Alanhou1222 <alanhou911222@gmail.com>
2020-06-04 12:59:22 +0800

    creative mode

---
+++ Assets/Script/Slime/CreativeMode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreativeMode : MonoBehaviour
+{
+    public float speed;
+    private float HmoveInput;
+    private float VmoveInput;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        HmoveInput = Input.GetAxisRaw("Horizontal");
+        VmoveInput = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(HmoveInput * speed, VmoveInput*speed);
+
+    }
+}
